d76055c849ea8c743b1d922ac094aff850e08e11
Oleg <olegdanilovic19@gmail.com>
2022-06-21 22:30:35 +0300

    adapter

--- js/script.js
+++ js/script.js
@@ -1,8 +1,38 @@
+
 $(document).ready(function(){
-    $('.slider').slick({
-        arrows: false,
-        dots: true,
-        //adaptiveHeight: true,
-        slidesToShow: 3
-    });
+  $('.slick').slick({
+    dots: true,
+    arrows: false,
+    infinite: true,
+  touchThreshold : 100,
+    speed: 300,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    
+  centerMode: true,
+  // nextArrow: '<button class="slick-next"><i class="fas fa-chevron-right"></i></button>',
+  // prevArrow: '<button class="slick-prev"><i class="fas fa-chevron-left"></i></button>',
+    responsive: [{
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+            }
+        },
+        {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }
+    ]
+  });
 })
